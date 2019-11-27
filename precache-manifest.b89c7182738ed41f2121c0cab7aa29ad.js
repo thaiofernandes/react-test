8df59bd06c0f1406a53dfdd36886cf982ce91af6
Thaio Fernandes <thaiofernandes@MacBook-Pro-de-Thaio.local>
2019-11-27 14:03:16 -0300

    Updates

---
+++ precache-manifest.b89c7182738ed41f2121c0cab7aa29ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86f72919ffc54b34f9e146d6ffcb104",
+    "url": "/thaiofernandes/react-test.git/index.html"
+  },
+  {
+    "revision": "99c2dd68e4b1763af7ba",
+    "url": "/thaiofernandes/react-test.git/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "34e30f6b7bd5ad11cf82",
+    "url": "/thaiofernandes/react-test.git/static/js/2.9b6246ed.chunk.js"
+  },
+  {
+    "revision": "99c2dd68e4b1763af7ba",
+    "url": "/thaiofernandes/react-test.git/static/js/main.25bf43f8.chunk.js"
+  },
+  {
+    "revision": "24fde5486e5170dc3214",
+    "url": "/thaiofernandes/react-test.git/static/js/runtime-main.3cf3d414.js"
+  }
+]);
